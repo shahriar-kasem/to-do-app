5f8031da02fe1a8f4fdea99dd6574d6bbd3369ac
Shahriar Kasem <shahriarkasem@gmail.com>
2022-06-04 10:27:39 +0600

    more validation and social login button added

--- src/components/shared/Login.js
+++ src/components/shared/Login.js
@@ -8,73 +8,77 @@ const Login = () => {
     const name = data.name;
     const email = data.email;
     const password = data.password;
-    const info = {name, email, password};
+    const info = { name, email, password };
     console.log(info)
     e.target.reset()
   };
 
   return (
-    <section className='text-center mt-5'>
+    <section className='text-center my-5 border-2 max-w-md rounded-xl mx-auto py-5'>
       <h1 className='text-lg text-info font-semibold'><i>Please login to continue</i></h1>
-     <div>
-     <form className='flex flex-col items-center' onSubmit={handleSubmit(onSubmit)}>
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Your
-              email</span>
-          </label>
-          <input type="email" placeholder="Email" autoComplete='off' class="input input-bordered w-full max-w-xs"
-            {...register("email", {
-              required: {
-                value: true,
-                message: 'Email is required'
-              },
-              pattern: {
-                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                message: 'Please enter a valid email'
-              }
-            })}
-          />
-          <label class="label">
-            {
-              errors?.email?.type === 'required' && <p className='text-red-500'><small>{errors.email.message}</small></p>
-            }
-            {
-              errors?.email?.type === 'pattern' && <p className='text-red-500'><small>{errors.email.message}</small></p>
-            }
-          </label>
-        </div>
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Your
-              password</span>
-          </label>
-          <input type="password" placeholder="Password" autoComplete='off' class="input input-bordered w-full max-w-xs"
-            {...register("password", {
-              required: {
-                value: true,
-                message: 'Password is required'
-              },
-              minLength: {
-                value: 8,
-                message: 'Your password must contain 8 character'
-              }
-            })}
-          />
-          <label class="label">
-            {
-              errors?.password?.type === 'required' && <p className='text-red-500'><small>{errors.password.message}</small></p>
-            }
-            {
-              errors?.password?.type === 'minLength' && <p className='text-red-500'><small>{errors.password.message}</small></p>
-            }
-          </label>
-        </div>
-        <button className='btn max-w-xs w-full btn-outline' type="submit">Login</button>
-      </form>
-     </div>
       <div>
-      <p><small>Dont have an account? <Link className='text-info' to='/signup'>Create account</Link></small></p>
+        <form className='flex flex-col items-center' onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Your
+                email</span>
+            </label>
+            <input type="email" placeholder="Email" autoComplete='off' className="input input-bordered w-full max-w-xs"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: 'Email is required'
+                },
+                pattern: {
+                  value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                  message: 'Please enter a valid email'
+                }
+              })}
+            />
+            <label className="label">
+              {
+                errors?.email?.type === 'required' && <p className='text-red-500'><small>{errors.email.message}</small></p>
+              }
+              {
+                errors?.email?.type === 'pattern' && <p className='text-red-500'><small>{errors.email.message}</small></p>
+              }
+            </label>
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Your
+                password</span>
+            </label>
+            <input type="password" placeholder="Password" autoComplete='off' className="input input-bordered w-full max-w-xs"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: 'Password is required'
+                },
+                minLength: {
+                  value: 8,
+                  message: 'Your password must contain 8 character'
+                }
+              })}
+            />
+            <label className="label">
+              {
+                errors?.password?.type === 'required' && <p className='text-red-500'><small>{errors.password.message}</small></p>
+              }
+              {
+                errors?.password?.type === 'minLength' && <p className='text-red-500'><small>{errors.password.message}</small></p>
+              }
+            </label>
+          </div>
+          <button  className={`btn max-w-xs w-full btn-outline ${(errors.name || errors.email || errors.password) ? 'btn-disabled' : ''}`} type="submit">Login</button>
+        </form>
+      </div>
+      <div>
+        <p><small>Dont have an account? <Link className='text-info' to='/signup'>Create account</Link></small></p>
+      </div>
+      <div className="divider max-w-xs mx-auto  font-semibold">OR</div>
+      <div>
+      <button className='btn max-w-xs w-full btn-outline' type="submit">Contine with Google</button>
       </div>
     </section>
   );
--- src/components/shared/SignUp.js
+++ src/components/shared/SignUp.js
@@ -4,99 +4,103 @@ import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = (data, e) => {
-    const name = data.name;
-    const email = data.email;
-    const password = data.password;
-    const info = {name, email, password};
-    console.log(info)
-    e.target.reset()
-  };
+    const onSubmit = (data, e) => {
+        const name = data.name;
+        const email = data.email;
+        const password = data.password;
+        const info = { name, email, password };
+        console.log(info)
+        // e.target.reset()
+    };
 
-  return (
-    <section className='text-center mt-5'>
-      <h1 className='text-lg text-info font-semibold'><i>Please Sign Up to continue</i></h1>
-     <div>
-     <form className='flex flex-col items-center' onSubmit={handleSubmit(onSubmit)}>
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Your
-              name</span>
-          </label>
-          <input type="text" placeholder="Name" autoComplete='off' class="input input-bordered w-full max-w-xs"
-            {...register("name", {
-              required: {
-                value: true,
-                message: 'Your name is required'
-              }
-            })}
-          />
-          <label class="label">
-            {
-              errors?.name?.type === 'required' && <p className='text-red-500'><small>{errors.name.message}</small></p>
-            }
-          </label>
-        </div>
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Your
-              email</span>
-          </label>
-          <input type="email" placeholder="Email" autoComplete='off' class="input input-bordered w-full max-w-xs"
-            {...register("email", {
-              required: {
-                value: true,
-                message: 'Email is required'
-              },
-              pattern: {
-                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                message: 'Please enter a valid email'
-              }
-            })}
-          />
-          <label class="label">
-            {
-              errors?.email?.type === 'required' && <p className='text-red-500'><small>{errors.email.message}</small></p>
-            }
-            {
-              errors?.email?.type === 'pattern' && <p className='text-red-500'><small>{errors.email.message}</small></p>
-            }
-          </label>
-        </div>
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Your
-              password</span>
-          </label>
-          <input type="password" placeholder="Password" autoComplete='off' class="input input-bordered w-full max-w-xs"
-            {...register("password", {
-              required: {
-                value: true,
-                message: 'Password is required'
-              },
-              minLength: {
-                value: 8,
-                message: 'Your password must contain 8 character'
-              }
-            })}
-          />
-          <label class="label">
-            {
-              errors?.password?.type === 'required' && <p className='text-red-500'><small>{errors.password.message}</small></p>
-            }
-            {
-              errors?.password?.type === 'minLength' && <p className='text-red-500'><small>{errors.password.message}</small></p>
-            }
-          </label>
-        </div>
-        <button className='btn max-w-xs w-full btn-outline' type="submit">Login</button>
-      </form>
-     </div>
-      <div>
-      <p><small>Already registered? <Link className='text-info' to='/login'>Please login</Link></small></p>
-      </div>
-    </section>
-  );
+    return (
+        <section className='text-center my-5 border-2 max-w-md rounded-xl mx-auto py-5'>
+            <h1 className='text-lg text-info font-semibold'><i>Please Sign Up to continue</i></h1>
+            <div>
+                <form className='flex flex-col items-center' onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Your
+                                name</span>
+                        </label>
+                        <input type="text" placeholder="Name" autoComplete='off' className="input input-bordered w-full max-w-xs"
+                            {...register("name", {
+                                required: {
+                                    value: true,
+                                    message: 'Your name is required'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {
+                                errors?.name?.type === 'required' && <p className='text-red-500'><small>{errors.name.message}</small></p>
+                            }
+                        </label>
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Your
+                                email</span>
+                        </label>
+                        <input type="email" placeholder="Email" autoComplete='off' className="input input-bordered w-full max-w-xs"
+                            {...register("email", {
+                                required: {
+                                    value: true,
+                                    message: 'Email is required'
+                                },
+                                pattern: {
+                                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                    message: 'Please enter a valid email'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {
+                                errors?.email?.type === 'required' && <p className='text-red-500'><small>{errors.email.message}</small></p>
+                            }
+                            {
+                                errors?.email?.type === 'pattern' && <p className='text-red-500'><small>{errors.email.message}</small></p>
+                            }
+                        </label>
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Your
+                                password</span>
+                        </label>
+                        <input type="password" placeholder="Password" autoComplete='off' className="input input-bordered w-full max-w-xs"
+                            {...register("password", {
+                                required: {
+                                    value: true,
+                                    message: 'Password is required'
+                                },
+                                minLength: {
+                                    value: 8,
+                                    message: 'Your password must contain 8 character'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {
+                                errors?.password?.type === 'required' && <p className='text-red-500'><small>{errors.password.message}</small></p>
+                            }
+                            {
+                                errors?.password?.type === 'minLength' && <p className='text-red-500'><small>{errors.password.message}</small></p>
+                            }
+                        </label>
+                    </div>
+                    <button className={`btn max-w-xs w-full btn-outline ${(errors.name || errors.email || errors.password) ? 'btn-disabled' : ''}`} type="submit">Sign Up</button>
+                </form>
+            </div>
+            <div>
+                <p><small>Already registered? <Link className='text-info' to='/login'>Please login</Link></small></p>
+            </div>
+            <div className="divider max-w-xs mx-auto  font-semibold">OR</div>
+            <div>
+                <button className='btn max-w-xs w-full btn-outline' type="submit">Contine with Google</button>
+            </div>
+        </section>
+    );
 };
 
 export default SignUp;
